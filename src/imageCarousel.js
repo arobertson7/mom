e5c0a9ffa4e29a4c87a5a5c80f016b1e393455c2
Austin Robertson <robertsonaustin27@gmail.com>
2025-05-12 17:10:16 -0400

    finished

--- src/imageCarousel.js
+++ src/imageCarousel.js
@@ -1,17 +1,20 @@
 import emptyNavDot from "./empty-dot.svg";
 import filledNavDot from "./filled-dot.svg";
 
+let slidesQueue = [];
+let currentSlideIndex = 0;
+
 const carousel = (function () {
     const next = function (slidesContainer, navDotContainer) {
         const slides = slidesContainer.querySelectorAll(".slide");
         // get current slide
-        let currentSlideIndex = 0;
-        while (
-            currentSlideIndex < slides.length &&
-            slides[currentSlideIndex].style.visibility != "visible"
-        ) {
-            currentSlideIndex++;
-        }
+        // let currentSlideIndex = 0;
+        // while (
+        //     currentSlideIndex < slides.length &&
+        //     slides[currentSlideIndex].style.visibility != "visible"
+        // ) {
+        //     currentSlideIndex++;
+        // }
         const currentSlide = slides[currentSlideIndex];
 
         // get index of next slide, wraps back to front if current is the last slide
@@ -25,21 +28,24 @@ const carousel = (function () {
         // give prev slide time to fade out before 'hidden'
         setTimeout(() => {
             currentSlide.style.visibility = "hidden";
-        }, 500);
+        }, 1500);
 
         updateNavDot(navDotContainer, currentSlideIndex, nextSlideIndex);
+
+        currentSlideIndex++;
+        currentSlideIndex = currentSlideIndex % slides.length;
     };
 
     const previous = function (slidesContainer, navDotContainer) {
         const slides = slidesContainer.querySelectorAll(".slide");
         // get current slide
-        let currentSlideIndex = 0;
-        while (
-            currentSlideIndex < slides.length &&
-            slides[currentSlideIndex].style.visibility != "visible"
-        ) {
-            currentSlideIndex++;
-        }
+        // let currentSlideIndex = 0;
+        // while (
+        //     currentSlideIndex < slides.length &&
+        //     slides[currentSlideIndex].style.visibility != "visible"
+        // ) {
+        //     currentSlideIndex++;
+        // }
         const currentSlide = slides[currentSlideIndex];
 
         // get index of prev slide, wraps back to end if current is the first slide
@@ -54,9 +60,12 @@ const carousel = (function () {
         // give prev slide time to fade out before 'hidden'
         setTimeout(() => {
             currentSlide.style.visibility = "hidden";
-        }, 500);
+        }, 1500);
 
         updateNavDot(navDotContainer, currentSlideIndex, prevSlideIndex);
+
+        currentSlideIndex--;
+        if (currentSlideIndex == -1) {currentSlideIndex = slides.length - 1}
     };
 
     const updateNavDot = function(navDotContainer, prevDotIndex, newDotIndex) {
@@ -65,7 +74,33 @@ const carousel = (function () {
         dots[newDotIndex].src = filledNavDot;
     }
 
-    return { next, previous };
+    // source takes:
+    //  1. 'next'
+    //  2. 'prev'
+    //  3. 'initialization' (skips the first two if's and only sets the the timeout at the end)
+    const slideShow = function(slidesContainer, navDotContainer, source) {
+        if (source == 'next') {
+            next(slidesContainer, navDotContainer);
+
+        }
+        else if (source == 'prev') {
+            previous(slidesContainer, navDotContainer);
+        }
+
+        // clear queue, removing upcoming timeouts, if there are any
+        for (let i = slidesQueue.length - 1; i >= 0; i--) {
+            clearTimeout(slidesQueue[i]);
+            slidesQueue.pop();
+        }
+
+        // set timeout for upcoming slide
+        const queuedSlide = setTimeout(() => {
+            slideShow(slidesContainer, navDotContainer, 'next');
+        }, 5000);
+        slidesQueue.push(queuedSlide);
+    }
+
+    return { next, previous, slideShow };
 })();
 
 export default carousel;
--- src/index.js
+++ src/index.js
@@ -1,68 +1,31 @@
 import "./styles.css";
-import dropDown from "./dropDown.js";
-import menuIcon from "./menu.svg";
-import menuDownIcon from "./menu-down.svg"
-
-import californiaImage from "./cali.jpg";
-import dresdenImage from "./dresden.jpg";
-import switzerlandImage from "./switzerland.jpg";
-import amsterdamImage from "./amsterdam.jpg";
-import nycImage from "./nyc.jpg";
-import parisImage from "./paris.jpg";
 
 import imageCarousel from "./imageCarousel.js";
 
-// event listeners for list options
-const californiaOption = document.querySelector('.california');
-californiaOption.addEventListener("click", () => {
-    const body = document.querySelector('body');
-    body.style.background = `url(${californiaImage})`;
-    body.style.backgroundSize = "cover";
-})
-const dresdenOption = document.querySelector('.dresden');
-dresdenOption.addEventListener("click", () => {
-    const body = document.querySelector('body');
-    body.style.background = `url(${dresdenImage})`;
-    body.style.backgroundSize = "cover";
-    body.style.backgroundPosition = "center";
-})
-const switzerlandOption = document.querySelector('.switzerland');
-switzerlandOption.addEventListener("click", () => {
-    const body = document.querySelector('body');
-    body.style.background = `url(${switzerlandImage})`;
-    body.style.backgroundSize = "cover";
-    body.style.backgroundPosition = "center";
-})
-const amsterdamOption = document.querySelector('.amsterdam');
-amsterdamOption.addEventListener("click", () => {
-    const body = document.querySelector('body');
-    body.style.background = `url(${amsterdamImage})`;
-    body.style.backgroundSize = "cover";
-    body.style.backgroundPosition = "center";
-})
-const parisOption = document.querySelector('.paris');
-parisOption.addEventListener("click", () => {
-    const body = document.querySelector('body');
-    body.style.background = `url(${parisImage})`;
-    body.style.backgroundSize = "cover";
-    body.style.backgroundPosition = "center";
-})
-const nycOption = document.querySelector('.nyc');
-nycOption.addEventListener("click", () => {
-    const body = document.querySelector('body');
-    body.style.background = `url(${nycImage})`;
-    body.style.backgroundSize = "cover";
-    body.style.backgroundPosition = "center";
-})
+
 
 // ------------ MAIN SCRIPT ------------ //
 
-const menuButton = document.getElementById("menu-button");
-const menu = document.getElementById("menu");
+const navMessage = document.getElementById("nav-message");
+setTimeout(() => {
+    navMessage.style.opacity = "0";
+}, 2000);
 
-// menuButton.addEventListener("click", function() {
-//     dropDown.toggle(menuButton, menu, menuIcon, menuDownIcon);
-// })
+setTimeout(() => {
+    navMessage.textContent = "Love, Austin ♥️";
+    navMessage.style.opacity = "1";
+}, 3000);
+
+setTimeout(() => {
+    navMessage.style.opacity = "0";
+}, 6000);
+
+setTimeout(() => {
+    navMessage.textContent = "Happy Mother's Day 💐";
+    navMessage.style.opacity = "1";
+}, 7000);
+
+//
 
 const slides = document.getElementById("carousel-slides-container");
 const navDotContainer = document.querySelector(".nav-dots");
@@ -70,9 +33,11 @@ const nextButton = document.querySelector(".next-slide-button");
 const prevButton = document.querySelector(".prev-slide-button");
 
 nextButton.addEventListener("click", function() {
-    imageCarousel.next(slides, navDotContainer);
+    imageCarousel.slideShow(slides, navDotContainer, 'next');
 })
 
 prevButton.addEventListener("click", function() {
-    imageCarousel.previous(slides, navDotContainer);
+    imageCarousel.slideShow(slides, navDotContainer, 'prev');
 })
+
+imageCarousel.slideShow(slides, navDotContainer, 'initialization');
